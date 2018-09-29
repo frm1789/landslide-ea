dfe37d8f300a973e80c603d5021cbd3a75a05c0e
FlorenciaMangini <florencia.mangini@gmail.com>
2018-09-29 17:43:05 -0500

    Adding styles

--- landslides_maps.R
+++ landslides_maps.R
@@ -1,5 +1,6 @@
 library(leaflet)
 library(dplyr)
+library(readr)
 
 url_land <- 'https://raw.githubusercontent.com/frm1789/landslide-ea/master/landslides.csv'
 df_land <- read_csv(url(url_land))
@@ -8,4 +9,7 @@ df <- df_land
 b <- leaflet(df) %>%
   addTiles() %>%
   addMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions())
-b
+
+b %>% addProviderTiles(providers$CartoDB.Positron)
+
+
